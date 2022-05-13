ab08944b43c4784598f457b565696dd411c5ff1b
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-13 13:25:49 +0530

    ci: delete project js-strings

--- packages/js-strings/src/lib/js-strings.spec.ts
+++
@@ -1,7 +0,0 @@
-import { jsStrings } from './js-strings';
-
-describe('jsStrings', () => {
-  it('should work', () => {
-    expect(jsStrings()).toEqual('js-strings');
-  });
-});
--- packages/js-strings/src/lib/js-strings.ts
+++
@@ -1,3 +0,0 @@
-export function jsStrings(): string {
-  return 'js-strings';
-}
